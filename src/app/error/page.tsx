dfd03b248b3298e237cd0b3177b11d61b9ffb619
Eric Frisk <125310875+ItsEricFrisk@users.noreply.github.com>
2025-02-03 19:12:45 +0100

    updated errorPage

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -2,26 +2,34 @@
 
 import Link from "next/link"
 import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
-
+import { useEffect, useState, Suspense } from "react"
 
 export default function ErrorPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ErrorPageContent />
+        </Suspense>
+    )
+}
 
+function ErrorPageContent() {
     const searchParams = useSearchParams()
-    const [errorMessage, setErrorMessage] = useState("An unkown error occurred.")
+    const [errorMessage, setErrorMessage] = useState("An unknown error occurred.")
 
     useEffect(() => {
         const message = searchParams?.get("message")
         if (message) {
             setErrorMessage(message)
         }
-    },[searchParams])
+    }, [searchParams])
 
     return (
         <section className="w-screen h-screen flex flex-col items-center justify-center">
             <h1 className="text-3xl">{errorMessage}</h1>
             <p className="text-xl mt-3">Return to homepage</p>
-            <Link href={"/"} className="bg-gradient-to-b from-slate-600 to-black text-white py-3 px-14 rounded-lg shadow-xl hover:shadow-none hover:scale-[.98] transition mt-6">Homepage</Link>
+            <Link href={"/"} className="bg-gradient-to-b from-slate-600 to-black text-white py-3 px-14 rounded-lg shadow-xl hover:shadow-none hover:scale-[.98] transition mt-6">
+                Homepage
+            </Link>
         </section>
     )
 }
